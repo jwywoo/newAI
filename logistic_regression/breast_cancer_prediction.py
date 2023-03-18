b3ccd4fe9f020b527b8a7205b652de7b052989d0
jwywoo <jwywoo26@gmail.com>
2023-03-18 18:08:45 -0400

    Logistic Regression Prac: Weight, bias and hypothesis done

--- logistic_regression/breast_cancer_prediction.py
+++ logistic_regression/breast_cancer_prediction.py
@@ -6,11 +6,12 @@ import pandas as pd
 # dataset url: https://www.kaggle.com/datasets/yasserh/breast-cancer-dataset
 
 # Reading csv using pandas
-csv_dataframe = pd.read_csv("breast-cancer.csv")
-dataframe_np = csv_dataframe.to_numpy()
+breast_csv = pd.read_csv("breast-cancer.csv")
+breast_np = breast_csv.to_numpy()
 
+# Preparing Values
 # y_values
-id_diagnosis = dataframe_np[:, :2]
+id_diagnosis = breast_np[:, :2]
 temp = list()
 for i in range(len(id_diagnosis)):
     if id_diagnosis[i][1] == "M":
@@ -19,5 +20,19 @@ for i in range(len(id_diagnosis)):
         temp.append(1)
 diagnosis_values = np.array(temp)
 # x_values
-symptoms = dataframe_np[:, 2:]
+symptoms = breast_np[:, 2:]
+
+# Weight and bias
+# Dimension of Weight
+# number of rows of weight got to be same as number of columns of X
+# number of columns of weight got to be same as number of columns of Y
+# Current number of columns are 32 thus W matrix got to be 32X1
+# print(len(csv_dataframe.columns))
+W = tf.Variable(tf.random.normal([len(breast_csv), 1]), name='Weight')
+b = tf.Variable(tf.random.normal([1]), name="Bias")
+
+
+# Hypothesis
+def hypoethesis(x):
+    return tf.sigmoid(tf.matmul(x, W) + b)
 
