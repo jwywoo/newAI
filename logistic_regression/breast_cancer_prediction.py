8ebed137abd4ac1e840fa51caf056dbf1fb25427
jwywoo <jwywoo26@gmail.com>
2023-03-18 17:39:28 -0400

    Logistic Regression Prac: Editing csv to dataframe and numpys

--- logistic_regression/breast_cancer_prediction.py
+++ logistic_regression/breast_cancer_prediction.py
@@ -5,4 +5,13 @@ import pandas as pd
 
 # Prediction for breast cancer
 # dataset url: https://www.kaggle.com/datasets/yasserh/breast-cancer-dataset
-print("hello world")
+
+# Reading csv using pandas
+csv_dataframe = pd.read_csv("breast-cancer.csv")
+dataframe_np = csv_dataframe.to_numpy()
+# print(dataframe_np)
+
+# y_values
+id_diagnosis = dataframe_np[:,:2]
+# x_values
+symptoms = dataframe_np[:,2:]
