66cf268211c43a39545f43009a59d84556b329ca
jwywoo <jwywoo26@gmail.com>
2023-03-18 17:59:49 -0400

    Logistic Regression Prac: x and y values are prepared

--- logistic_regression/breast_cancer_prediction.py
+++ logistic_regression/breast_cancer_prediction.py
@@ -2,16 +2,22 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 
-
 # Prediction for breast cancer
 # dataset url: https://www.kaggle.com/datasets/yasserh/breast-cancer-dataset
 
 # Reading csv using pandas
 csv_dataframe = pd.read_csv("breast-cancer.csv")
 dataframe_np = csv_dataframe.to_numpy()
-# print(dataframe_np)
 
 # y_values
-id_diagnosis = dataframe_np[:,:2]
+id_diagnosis = dataframe_np[:, :2]
+temp = list()
+for i in range(len(id_diagnosis)):
+    if id_diagnosis[i][1] == "M":
+        temp.append(0)
+    else:
+        temp.append(1)
+diagnosis_values = np.array(temp)
 # x_values
-symptoms = dataframe_np[:,2:]
+symptoms = dataframe_np[:, 2:]
+
