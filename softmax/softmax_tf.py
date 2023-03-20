4eba44d32b08d6a4fcb1349fce0d156d26f4c7c3
jwywoo <jwywoo26@gmail.com>
2023-03-20 17:03:53 -0400

    Softmax classfication: hypothesis and data done

--- softmax/softmax_tf.py
+++ softmax/softmax_tf.py
@@ -1,4 +1,38 @@
 import tensorflow as tf
 import numpy as np
 
-# Start
+# practice data
+# x_data: 8X4
+# y_data: 8X3
+x_data = [[1, 2, 1, 1],
+          [2, 1, 3, 2],
+          [3, 1, 3, 4],
+          [4, 1, 5, 5],
+          [1, 7, 5, 5],
+          [1, 2, 5, 6],
+          [1, 6, 6, 6],
+          [1, 7, 7, 7]]
+y_data = [[0, 0, 1],
+          [0, 0, 1],
+          [0, 0, 1],
+          [0, 1, 0],
+          [0, 1, 0],
+          [0, 1, 0],
+          [1, 0, 0],
+          [1, 0, 0]]
+
+x_data = np.array(x_data, dtype=np.float32)
+y_data = np.array(y_data, dtype=np.float32)
+
+# number of columns of y_data
+nb_classes = 3
+
+# Weight and Bias
+# number of rows of weight got to be same number of column of x_data
+# number of column of weight got to be same as y_data's column
+W = tf.Variable(tf.random.normal([4,nb_classes]), name="weight")
+# bias's rows and columns got to be same as multiplication of x_data and weight
+b = tf.Variable(tf.random.normal([nb_classes]), name='bias')
+
+# Hypothesis
+hypothesis = tf.nn.softmax(tf.matmul(x_data,W) + b)
