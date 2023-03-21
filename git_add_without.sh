3b94f853ecf1589dfee6d23febb4de1c8fb81e4d
jwywoo <jwywoo26@gmail.com>
2023-03-21 13:11:44 -0400

    Softmax practice: start

--- git_add_without.sh
+++ git_add_without.sh
@@ -2,4 +2,5 @@ echo add everything without breast-cancer-dataset.csv
 git status
 git add -A
 git reset logistic_regression/breast-cancer.csv
+git reset softmax/images
 git status
--- git_push.sh
+++ git_push.sh
@@ -1,4 +1,5 @@
 echo git push origin master
 git reset logistic_regression/breast-cancer.csv
+git reset softmax/images
 git status
 git push origin master
